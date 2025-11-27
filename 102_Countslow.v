c7546dc2975495aa8176d20c5f0517ef8ec2ccda
kooksh2906-dev <kooksh2906@gmail.com>
2025-11-27 19:22:16 +0900

    Solution for Countslow

---
+++ 102_Countslow.v
@@ -0,0 +1,17 @@
+module top_module (
+    input clk,
+    input slowena,
+    input reset,
+    output [3:0] q
+);
+
+    always@(posedge clk) begin
+        if(reset)
+            q <= 4'd0;
+        else if(slowena)
+            q <= (q == 4'd9)? 4'd0: q+1'b1;
+        else
+            q<=q;
+    end
+    
+endmodule
