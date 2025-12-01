8927c772f78a335a1d7529cca462b1a79b11f6e1
kooksh2906-dev <kooksh2906@gmail.com>
2025-12-01 22:19:21 +0900

    Solution for Shift4

---
+++ 107_Shift4.v
@@ -0,0 +1,24 @@
+module top_module(
+    input clk,
+    input areset,  // async active-high reset to zero
+    input load,
+    input ena,
+    input [3:0] data,
+    output reg [3:0] q
+);
+    always@(posedge clk, posedge areset) begin
+        if(areset)
+            q <= 4'b0;
+        else if(load)
+            q <= data;
+        else if(ena) begin
+            q <= {1'b0,q[3:1]};
+            //or
+            //q[2:0] <= q[3:1];
+            //q[3] <= 1.b0;
+        end
+        else
+            q <= q;
+    end
+           
+endmodule
